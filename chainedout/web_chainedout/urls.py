ed8108641b072e8419591a6ff7e634cd92f9d41f
Denysov Alex <32078697+dekinsg@users.noreply.github.com>
2019-09-27 00:45:37 +0200

    Fixed login url and added logout

--- chainedout/web_chainedout/urls.py
+++ chainedout/web_chainedout/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('register/', views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name='logout'),
 ]
