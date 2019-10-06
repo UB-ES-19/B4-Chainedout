36839e7406aab56126c1aad344b21ffc8b9bf213
manuMatnez <mmartima285@alumnes.ub.edu>
2019-10-06 03:49:26 +0200

    script.js * user/profile.html w comments

--- chainedout/static/style/script.js
+++ chainedout/static/style/script.js
@@ -1,12 +1,25 @@
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	var modals = [document.getElementById('login_modal'), 
-	document.getElementById('profile_modal'),
-	document.getElementById('about_modal'), 
-	document.getElementById('xp_modal'),
-	document.getElementById('skills_modal'), 
-	document.getElementById('achievements_modal')];
+	var modals = [
+	document.getElementById('login_modal'), 
+	document.getElementById('profile_user_edit_modal'),
+	document.getElementById('profile_about_modal'),
+	document.getElementById('profile_edu_add_modal'),
+	document.getElementById('profile_edu_edit_modal'),
+	document.getElementById('profile_xp_add_modal'),
+	document.getElementById('profile_xp_edit_modal'),
+	document.getElementById('profile_skills_add_modal'),
+	document.getElementById('profile_skills_edit_modal'), 
+	document.getElementById('profile_publication_modal'),
+	document.getElementById('profile_patent_modal'),
+	document.getElementById('profile_course_modal'),
+	document.getElementById('profile_project_modal'),
+	document.getElementById('profile_awards_modal'),
+	document.getElementById('profile_test_q_modal'),
+	document.getElementById('profile_lang_modal'),
+	document.getElementById('profile_company_modal')
+	];
 	
 	for (i = 0; i < modals.length; i++) {
 		if (event.target == modals[i]) {
