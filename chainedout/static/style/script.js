904b1cead175cdda67c63f015bd7c1ec89c02699
manuMatnez <mmartima285@alumnes.ub.edu>
2019-10-06 02:23:14 +0200

    script js

--- chainedout/static/style/script.js
+++ chainedout/static/style/script.js
@@ -1,14 +1,22 @@
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  var modal = document.getElementById('login_modal');
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	var modals = [document.getElementById('login_modal'), 
+	document.getElementById('profile_modal'),
+	document.getElementById('about_modal'), 
+	document.getElementById('xp_modal'),
+	document.getElementById('skills_modal'), 
+	document.getElementById('achievements_modal')];
+	
+	for (i = 0; i < modals.length; i++) {
+		if (event.target == modals[i]) {
+			modals[i].style.display = "none";
+		}
+	}
 }
 
-// shows/hides navigation menu
-function loginModal(id, type) {
-    var modal = document.getElementById(id);
-    modal.style.display = type;
+// shows/hides modal
+function modal(id, type) {
+	var modal = document.getElementById(id);
+	modal.style.display = type;
 }
