31f35641a6910c823da9aa7660b2d333fd9c3ee3
crpantsal <crpantas7@alumnes.ub.edu>
2019-11-26 23:40:24 +0100

    Front comment

--- chainedout/static/style/script.js
+++ chainedout/static/style/script.js
@@ -23,3 +23,18 @@ function modal(id, type) {
 	var modal = document.getElementById(id);
 	modal.style.display = type;
 }
+
+
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+        this.rows = minRows + rows;
+    });
