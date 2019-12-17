60822bba21c17fb8c594e96af29eea2e808f9920
Denysov Alex <32078697+dekinsg@users.noreply.github.com>
2019-12-17 22:30:31 +0100

    Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chainedout.wsgi --log-file -
--- chainedout/chainedout/settings.py
+++ chainedout/chainedout/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fsbysrmyg(cr_r&eh2c#5c6rj74a7$utp8b(oc!o^ak5popdpi'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fsbysrmyg(cr_r&eh2c#5c6rj74a7$utp8b(oc!o^ak5popdpi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,7 +87,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'carloshui95',
         'HOST': '',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
@@ -128,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -153,3 +155,12 @@ REST_FRAMEWORK = {
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_info', args=[u.username])
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
