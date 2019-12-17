a50ca204a45aecd37eda68f1b229d875d057c73f
Denysov Alex <32078697+dekinsg@users.noreply.github.com>
2019-12-17 22:52:48 +0100

    Update settings.py

--- chainedout/chainedout/settings.py
+++ chainedout/chainedout/settings.py
@@ -133,7 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'var/www/static',
 )
 
 MEDIA_URL = '/media/'
